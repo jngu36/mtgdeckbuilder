0db30540809e5363e6d88407b3646389dc1ee9cf
jnguyen36 <myvteddy@gmail.com>
2024-04-18 14:04:15 -0400

    PRAY YOU #@$*

--- pages/lib/connectDB.js
+++ pages/lib/connectDB.js
@@ -1,5 +1,19 @@
+/**
 import mongoose from "mongoose";    
 
-const connectDB = async () => mongoose.connect(process.env.MONGO);
+const connectDB = async () => await mongoose.connect(process.env.MONGO);
 
-export default connectDB;
+export default connectDB; */
+
+import mongoose from "mongoose";
+
+const connectDb = (handler) => async (req, res) => {
+    if (mongoose.connections[0].readyState) {
+        return handler(req, res);
+    }
+    
+    await mongoose.connect(process.env.MONGO);
+    return handler(req, res);
+};
+
+export default connectDb;
