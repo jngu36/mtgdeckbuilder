5dc45ff0e9e055ae926c36069232d738e29227d8
jnguyen36 <myvteddy@gmail.com>
2024-04-18 15:23:41 -0400

    updatedeck changes

--- pages/api/updateDeck.js
+++ pages/api/updateDeck.js
@@ -8,17 +8,16 @@ export default async function handler(req, res) {
         const description = req.body.description ? req.body.description : "This is a deck. There are many like it but this one is mine.";
 
         if (cards && id && name) {
-            const list = { $set: {cards: cards, name: name, description: description} };
-            const condition = { _id: id };
+            const change = { $set: {cards: cards, name: name, description: description} };
+            const find = { _id: id };
 
             await mongoose.connect(process.env.MONGO);
-            await Deck.findOneAndUpdate(condition, list);
+            await Deck.findOneAndUpdate(find, change);
 
             res.status(200).json({ message: "Deck saved!" });
         } else {
             res.status(500).json({ message: "We have no idea what went wrong." });
         }
-
     } catch (error) {
         console.log(error);
         res.status(500).json({ error: 'Something wrong happened' })
